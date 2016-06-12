b7c7ff7e8bd87884abdfe9f30acbfa1393de4d8a
Lucas Steinmann <lucas.steinmann@gmail.com>
2016-06-12 18:58:37 +0200

    Deleted old view. Reinserted graphmodel package to uml diagram.

--- entwurf/papyrus/gansmodel/src/graphmodel/DefaultVertex.java
+++ entwurf/papyrus/gansmodel/src/graphmodel/DefaultVertex.java
@@ -1,5 +1,17 @@
 package graphmodel;
 
-public class DefaultVertex { 
+public class DefaultVertex implements Vertex {
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Integer getID() {
+		// TODO Auto-generated method stub
+		return null;
+	} 
 
 }
--- entwurf/papyrus/gansmodel/src/graphmodel/DirectedEdge.java
+++ entwurf/papyrus/gansmodel/src/graphmodel/DirectedEdge.java
@@ -1,5 +1,31 @@
 package graphmodel;
 
-public class DirectedEdge { 
+import graphmodel.Graph.V;
+
+public class DirectedEdge implements Edge {
+
+	@Override
+	public V getSource() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public V getTarget() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Integer getID() {
+		// TODO Auto-generated method stub
+		return null;
+	} 
 
 }
