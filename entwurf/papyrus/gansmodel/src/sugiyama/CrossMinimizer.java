d670b1728f1c07a53768455de0a96972df0507fb
Jonas Meier <meierjonas96@web.de>
2016-06-13 15:31:43 +0200

    Added JavaDoc to sugiyama.CrossMinimizer and
    changed parameter type of graph from minimizeCrossings from DirectedGraph<G, V, E> to G.

--- entwurf/papyrus/gansmodel/src/sugiyama/CrossMinimizer.java
+++ entwurf/papyrus/gansmodel/src/sugiyama/CrossMinimizer.java
@@ -4,9 +4,17 @@ import graphmodel.DirectedEdge;
 import graphmodel.DirectedGraph;
 import graphmodel.Vertex;
 
+/**
+ * Takes a graph and rearranges its vertices.
+ */ 
 public class CrossMinimizer<G extends DirectedGraph<G, V, E>, V extends Vertex, E extends DirectedEdge<V>> {
 
 	public CrossMinimizer() { }
-
-	public void minimizeCrossings(DirectedGraph<G, V, E> graph ) { }
+	
+	/**
+	 * Rearranges vertices in the graph argument in order to remove the amount of crosses of their edges.
+	 * 
+	 * @param graph input graph
+	 */ 
+	public void minimizeCrossings(G graph ) { }
 }
