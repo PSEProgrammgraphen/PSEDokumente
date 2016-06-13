36340154fb47e1a24ad8662dc94a1bdcdfc9b5a6
Jonas Meier <meierjonas96@web.de>
2016-06-13 14:33:00 +0200

    Added JavaDoc to sugiyama.DAGMaker.java and
    changed type of set from Vertex to DirectedEdge

--- entwurf/papyrus/gansmodel/src/sugiyama/DAGMaker.java
+++ entwurf/papyrus/gansmodel/src/sugiyama/DAGMaker.java
@@ -4,7 +4,6 @@ import java.util.Set;
 
 import graphmodel.DirectedEdge;
 import graphmodel.DirectedGraph;
-import graphmodel.Vertex;
 
 /**
  * Takes a directed Graph G = (V, E) and removes a set of edges E_ so that the resulting Graph G' = (V, E\E_) is a DAG.
@@ -18,7 +17,14 @@ public class DAGMaker<G extends DirectedGraph<G, V, E>, V extends Vertex, E exte
 	public DAGMaker() {
 	}
 
-	public Set<Vertex> removeCycles(DirectedGraph<G, V, E> graph) {
+	/**
+	 * Searches for the maximum acyclic subgraph in the graph argument and invertes the direction of the edges that
+	 * are not part of this subgraph.
+	 * 
+	 * @param graph the input graph to remove cycles from
+	 * @return a set of edges whose direction has been inverted in order to remove cycles from the graph
+	 */
+	public Set<DirectedEdge> removeCycles(DirectedGraph<G, V, E> graph) {
 		return null;
 	}
 }
