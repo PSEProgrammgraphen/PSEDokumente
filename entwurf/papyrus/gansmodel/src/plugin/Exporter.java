7daf41a5ebdcfb5394a8ac50343edb1ad0040d25
Sven Kummetz <kummetz.sven@gmail.com>
2016-06-15 23:31:02 +0200

    adds Exporter

---
+++ entwurf/papyrus/gansmodel/src/plugin/Exporter.java
@@ -0,0 +1,36 @@
+package plugin;
+
+import graphmodel.IGraphModelBuilder;
+import java.io.FileOutputStream;
+
+/**
+ * The importer interface is used to import a graph from a file into the intern representation.
+ * The main task is to parse a FileInputStream  into the Interface of an {@link AbstractGraphModelBuilder}.
+ * The {@link AbstractGraphModelBuilder} will then build the representation. 
+ *
+ */
+public interface Importer {
+
+	/**
+	 * Get all filetypes which this importer can parse
+	 * 
+	 * @return 
+	 */
+	public String getSupportedFileEndings();
+
+	/**
+	 * Get the name of this importer
+	 * 
+	 * @return 
+	 */
+	public String getName();
+
+	/**
+	 * This method writes an {@link SerializedGraph} into an FileOutputStream.
+	 * 
+	 * @param graph
+	 * @param filestream 
+	 */
+	public void exportGraph(SerializedGraph graph, FileOutputStream filestream); 
+
+}
