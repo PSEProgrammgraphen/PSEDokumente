1faab33363873b3e6416fc3d355375d1f0960476
Jonas Meier <meierjonas96@web.de>
2016-06-16 10:33:23 +0200

    Removed SugiyamaPath, moved
    SugiyamaVertex and SugiyamaEdge as nested classes to SugiyamaGraph

--- entwurf/papyrus/gansmodel/src/sugiyama/SugiyamaGraph.java
+++ entwurf/papyrus/gansmodel/src/sugiyama/SugiyamaGraph.java
@@ -5,6 +5,7 @@ import graphmodel.SerializedGraph;
 import graphmodel.DefaultVertex;
 import graphmodel.DirectedEdge;
 import graphmodel.Vertex;
+import sugiyama.SugiyamaGraph.SupplementPath;
 
 import java.util.List;
 import java.util.Map;
@@ -152,7 +153,32 @@ public class SugiyamaGraph<G extends DirectedGraph<V, E>, V extends Vertex, E ex
 	 * A supplement vertex which is part of a {@link SupplementPath}.
 	 */
 	public class SupplementVertex extends DefaultVertex {}
+	
+	
+	public class SugiyamaVertex<V extends Vertex> extends DefaultVertex
+	{
+		public boolean isDummyVertex() {return false;}
+		public V getVertex() {return null;}
+		
+	}
 
+	
+	/**
+	 * A wrapper class for directed edges to implement additional functionality
+	 * to apply the sugiyama layout to the SugiyamaGraph containing them.
+	 *
+	 * @param <E> the type of the edge which should be wrapped
+	 * @param <V> the type of the vertex the E connects
+	 */
+	public class SugiyamaEdge<E extends DirectedEdge<V>, V extends Vertex> extends DirectedEdge<SugiyamaVertex<V>>
+	{
+		private SugiyamaEdge(E edge) {}	
+		public E getEdge() { return null; }
+		private boolean isReversed() { return false; }
+		private void setReversed(boolean rev) {}
+		private boolean isReplaced() { return false; }
+		private List<SupplementPath> getSupplement() { return null; }
+	}
 	
 	@Override
 	public SerializedGraph serialize() {
--- entwurf/papyrus/gansmodel/src/sugiyama/SugiyamaPath.java
+++
@@ -1,3 +0,0 @@
-public class SugiyamaPath<V extends Vertex>
-{
-}
