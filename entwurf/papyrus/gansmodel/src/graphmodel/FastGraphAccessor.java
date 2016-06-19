4c3f88e856448a7476543934904bf61e0f0dd604
Sven Kummetz <kummetz.sven@gmail.com>
2016-06-20 01:50:54 +0200

    adds javaDoc to FastGraphAccessor

--- entwurf/papyrus/gansmodel/src/graphmodel/FastGraphAccessor.java
+++ entwurf/papyrus/gansmodel/src/graphmodel/FastGraphAccessor.java
@@ -4,71 +4,80 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * 
- * @author
+ * This class provides a fast lookup of {@link Vertex} and {@link Edge} for a given Attribute value pair without traversing a {@link Graph}.
+ * FastGraphAccessor is a helper class for looking up all {@link Vertex} and {@link Edge} that have a specific value
+ * for a specific attribute. To achieve this all elements of a Graph need to add their attributes and values to a
+ * FastGraphAccessor. These values are not linked to the origin values so the fastGraphAccessor needs to be updated
+ * after changes when needed for following steps. This should be done by reverting and adding the values again.
  */
 public class FastGraphAccessor {
-
 	/**
-	 * 
+	 * Adds an {@link Edge} for a given attribute with a given value.
+	 * @param name name of the attribute
+	 * @param value value of the attribute
+	 * @param edge edge that has this value for the given attribute
 	 */
-	private Map edgeValues;
-	/**
-	 * 
-	 */
-	private Map vertexValues;
-	/**
-	 * 
-	 */
-	private Graph graph;
-
-	/**
-	 * 
-	 * @param value
-	 * @param name
-	 * @return
-	 */
-	public List<Edge> getEdgesByAttribute(String value, String name) {
-		// TODO Auto-generated method
-		return null;
-	}
-
-	/**
-	 * 
-	 * @param value
-	 * @param name
-	 * @return
-	 */
-	public List<Edge> getEdgesByAttribute(String value, int name) {
-		// TODO Auto-generated method
-		return null;
-	}
-
-	/**
-	 * 
-	 * @param name
-	 * @param edge
-	 * @param value
-	 */
-	public void addEdgeForAttribute(String name, Edge edge, String value) {
+	public void addEdgeForAttribute(String name, String value, Edge edge) {
 		// TODO Auto-generated method
 	}
 
 	/**
-	 * 
-	 * @param name
-	 * @param edge
-	 * @param value
+	 * Adds an {@link Edge} for a given attribute with a given value.
+	 * @param name name of the attribute
+	 * @param value value of the attribute
+	 * @param edge edge that has this value for the given attribute
 	 */
 	public void addEdgeForAttribute(String name, Edge edge, int value) {
 		// TODO Auto-generated method
 	}
 
 	/**
-	 * 
-	 * @param name
-	 * @param value
-	 * @return
+	 * gets a List of {@link Edge} that contains all {@link Edge} that have the value for given attribute
+	 * @param name name of the attribute
+	 * @param value value of the attribute
+	 * @return a {@link List} of {@link Edge} that has tthe value for given attribute
+	 */
+	public List<Edge> getEdgesByAttribute(String name, String value) {
+		// TODO Auto-generated method
+		return null;
+	}
+
+	/**
+	 * gets a {@link List} of {@link Edge} that contains all {@link Edge} that have the value for given attribute
+	 * @param name name of the attribute
+	 * @param value value of the attribute
+	 * @return a {@link List} of {@link Edge} that has the value for given attribute
+	 */
+	public List<Edge> getEdgesByAttribute(String name, int value) {
+		// TODO Auto-generated method
+		return null;
+	}
+
+	/**
+	 * adds an {@link Vertex} for a given attribute with a given value
+	 * @param name name of the attribute
+	 * @param value value of the attribute
+	 * @param vertex vertex that has this value for the given attribute
+	 */
+	public void addVertexForAttribute(Vertex vertex, String value, String name) {
+		// TODO Auto-generated method
+	}
+
+	/**
+	 * adds an {@link Vertex} for a given attribute with a given value
+	 * @param name name of the attribute
+	 * @param value value of the attribute
+	 * @param vertex vertex that has this value for the given attribute
+	 */
+	public void addVertexForAttribute(Vertex vertex, String value, int name) {
+		// TODO Auto-generated method
+	}
+
+	/**
+	 * gets a {@link List} of {@link Vertex} that contains all {@link Vertex} that have the value for given attribut
+	 * @param name name of the attribute
+	 * @param value value of the attribute
+	 * @return a {@link List} of {@link Vertex} that has the value for given attribute
 	 */
 	public List<Vertex> getVerticesByAttribute(String name, String value) {
 		// TODO Auto-generated method
@@ -76,10 +85,10 @@ public class FastGraphAccessor {
 	}
 
 	/**
-	 * 
-	 * @param name
-	 * @param value
-	 * @return
+	 * gets a {@link List} of {@link Vertex} that contains all {@link Vertex} that have the value for given attribut
+	 * @param name name of the attribute
+	 * @param value value of the attribute
+	 * @return a {@link List} of {@link Vertex} that has the value for given attribute
 	 */
 	public List<Vertex> getVerticesByAttribute(String name, int value) {
 		// TODO Auto-generated method
@@ -87,27 +96,9 @@ public class FastGraphAccessor {
 	}
 
 	/**
-	 * 
-	 * @param vertex
-	 * @param value
-	 * @param name
-	 */
-	public void addVertexForAttribute(Vertex vertex, String value, String name) {
-		// TODO Auto-generated method
-	}
-
-	/**
-	 * 
-	 * @param vertex
-	 * @param value
-	 * @param name
-	 */
-	public void addVertexForAttribute(Vertex vertex, String value, int name) {
-		// TODO Auto-generated method
-	}
-
-	/**
-	 * 
+	 * Deletes all data in this FastGraphAccessor.
+	 * After this step all the information needs to be readded to this.
+	 * This is necessary when updating the FastGraphAccessor
 	 */
 	public void reset() {
 		// TODO Auto-generated method
