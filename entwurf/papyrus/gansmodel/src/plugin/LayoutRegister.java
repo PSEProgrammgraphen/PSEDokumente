649c942cd41f31d8dfb1518c898c9208f45440ed
Lucas Steinmann <lucas.steinmann@gmail.com>
2016-06-20 03:51:05 +0200

    Added JavaDoc to LayoutRegister and Generics to VertexFilter.

--- entwurf/papyrus/gansmodel/src/plugin/LayoutRegister.java
+++ entwurf/papyrus/gansmodel/src/plugin/LayoutRegister.java
@@ -1,16 +1,22 @@
-/**
- * 
- */
 package plugin;
 
 import java.util.List;
 
 /**
- * Stores a collection of layouts.
- * This 
+ * Stores a collection of layouts for a specific graph.
+ * This allows the client to select fitted layouts for every graph.
  */
 public interface LayoutRegister<E extends LayoutOption> {
 	
+	/**
+	 * Adds a layout option to this register.
+	 * @param option the layout option
+	 */
 	public void addLayoutOption(E option);
+	
+	/**
+	 * Returns all available layouts for the graph associated with this register.
+	 * @return the available layouts
+	 */
 	public List<LayoutOption> getLayoutOptions();
 }
