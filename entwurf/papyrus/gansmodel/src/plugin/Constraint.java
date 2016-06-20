eca5bef3cd338ebb899b261f74e3f729f1386ed2
Lucas Steinmann <lucas.steinmann@gmail.com>
2016-06-20 04:17:57 +0200

    Added JavaDoc and removed orphaned type.

--- entwurf/papyrus/gansmodel/src/plugin/Constraint.java
+++ entwurf/papyrus/gansmodel/src/plugin/Constraint.java
@@ -1,5 +1,14 @@
 package plugin;
 
+/**
+ * The most abstract interface for a constraint.
+ * Can later be used to achieve compatibility between constraints of different domains and layouts.
+ */
 public interface Constraint { 
+	
+	/**
+	 * The name of the constraint
+	 * @return the name
+	 */
 	public String getName();
 }
